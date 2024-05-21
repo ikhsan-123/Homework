130ac8d825bc82a7280ba145101d2a6ba9b5f0ed
TOSHIBA <muhhavidsikhsan@gmail.com>
2024-05-21 09:50:08 +0700

    nambah cihuy

--- exercise.py
+++ exercise.py
@@ -6,12 +6,34 @@ def tambah():
     stok.append(barang)
     return "--- Data berhasil ditambahkan ---"
 
+def edit():
+    nama = input('Masukkan barang yang ingin diubah: ')
+    if nama not in stok:
+        print(f'Barang {nama} tidak tersedia')
+        return
+
+    print(f"List Barang Saat Ini '{nama}':")
+    print(f"\tJumlah: {stok[nama][0]}")
+
+    pilihan = input('Pilihan Ubah\n1. Jumlah\n2. Batal')
+    if pilihan == 1:
+        jumlah = int(input('Masukkan Jumlah: '))
+        stok[nama][0] = jumlah
+        print(f'Jumlah {nama} telah diubah')
+    elif pilihan == 2:
+        exit
+  
 def hapus():
     delete = int(input("Hapus data index ke : "))
     delete -= 1
     del stok[delete]
     return "--- Data berhasil dihapus ---"
-
+def search():
+    lihat = input('Cari Barang: ')
+    if lihat in stok:
+        print(f'Barang {lihat} tersedia {stok[lihat]} pcs')
+    else:
+        print(f'Barang {lihat} tidak tersedia')
 def tampil():
     print("--- Data barang ---")
     number = 1
@@ -19,3 +41,12 @@ def tampil():
         print(f"{number}. {i["nama"]} , Stok : {i["stok"]}")
         number += 1
     return ""
+def view_item_cout():
+    if not stok:
+        print('Tidak ada data')
+        return
+    print(f"List Barang Saat Ini:")
+    for nama,data in stok.items():
+        kuantitas = data
+        print(f'{nama}: ')
+        print(f'\tJumlah: {kuantitas}')
--- modul.py
+++ modul.py
@@ -3,9 +3,10 @@ import exercise as mod
 print("Selamat datang di program manajemen stock barang!")
 print("Pilihan :")
 print("1. Tambah data barang")
-print("2. Hapus data barang")
-print("3. Tampilkan data barang")
-print("4. Keluar")
+print("2. edit data")
+print("3. Hapus data barang")
+print("4. Tampilkan data barang")
+print("5. Keluar")
 print('\n')
 
 while True:
@@ -15,9 +16,18 @@ while True:
         print(mod.tambah())
         print("")
     elif pilihan == 2:
-        print(mod.hapus())
+        print(mod.edit())
         print("")
     elif pilihan == 3:
-        print(mod.tampil())
+        print(mod.hapus())
+        print("")
     elif pilihan == 4:
+        print(mod.search())
+        print("")
+    elif pilihan == 5:
+        print(mod.tampil())
+        print("")
+    elif pilihan == 6:
+        print(mod.view_item_cout)
+    elif pilihan == 7:
         exit()
